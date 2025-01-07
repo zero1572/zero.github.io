fc0a8859f545963b9f6123bb3a09d69356e5308a
菜狗 <115909711+zero1572@users.noreply.github.com>
2025-01-07 21:15:14 +0800

    Site updated: 2025-01-07 21:15:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/25/hello-world/"];function toRandomPost(){
+var posts=["2024/12/25/hello-world/","2025/01/07/new-title/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
